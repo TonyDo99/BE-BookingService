d4c9f756ddcf13e5de4f05b82f9cd50399158b7a
Tony Do <dotanphat20@gmail.com>
2022-12-19 10:51:18 +0700

    check cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: '*',
   });
   await app.listen(4000, () =>
     console.log('Server now listening on port 4000'),
