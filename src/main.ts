97bbb16ae0a134b0a90df870d1cdcc4f9b61885e
Tony Do <dotanphat20@gmail.com>
2022-12-19 11:00:08 +0700

    config cros domain

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://booking-service-fe.vercel.app/'],
   });
   await app.listen(4000, () =>
     console.log('Server now listening on port 4000'),
