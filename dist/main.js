023c1228d6553be693dd9f3c1866ebc604a27094
Tony Do <dotanphat20@gmail.com>
2022-12-19 11:25:00 +0700

    cors protect

--- dist/main.js
+++ dist/main.js
@@ -4,11 +4,11 @@ const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.setGlobalPrefix('api');
     app.enableCors({
-        origin: ['http://localhost:3000'],
+        origin: ['http://localhost:3000', 'https://booking-service-fe.vercel.app'],
     });
     await app.listen(4000, () => console.log('Server now listening on port 4000'));
 }
